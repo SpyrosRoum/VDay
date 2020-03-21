6a884ed038ef2eba015bc5e70c3e0c907e79b6ed
SpyrosRoum <spyrosr1@gmail.com>
2020-03-21 02:33:08 +0200

    Make monsters actually attack player and deal dmg

--- vday/components/ai.py
+++ vday/components/ai.py
@@ -1,11 +1,16 @@
 import tcod
 
+from entity import Entity
+
 class BasicMonster:
+    def __init__(self):
+        self.owner: Entity
+
     def take_turn(self, target, fov_map, game_map, entities):
         monster = self.owner
 
         if tcod.map_is_in_fov(fov_map, monster.x, monster.y):
             if monster.distance_to(target) >= 2:
                 monster.move_towards(target.x, target.y, game_map, entities)
-            elif target.fighter.hp > 0:
-                print(f'The {monster.name} hit you')
+            else:
+                monster.fighter.attack(target)
