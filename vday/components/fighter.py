12792ac46ac11486bc0b2fb93d51f107ce2508e2
SpyrosRoum <spyrosr1@gmail.com>
2020-03-21 02:03:16 +0200

    Add doing damage

--- vday/components/fighter.py
+++ vday/components/fighter.py
@@ -1,6 +1,27 @@
+from entity import Entity
+
 class Fighter:
     def __init__(self, hp, defense, power):
         self.max_hp = hp
         self.hp = hp
         self.defense = defense
         self.power = power
+
+    def take_dmg(self, from_, amount):
+        print(f"{self.owner.name} got {amount} damage from {from_.owner.name}")
+
+        self.hp -= amount
+
+        if self.hp <= 0 and self.owner.name != 'V':
+            print(f"The {self.owner.name} died")
+        elif self.hp <= 0:
+            # TODO: handle player death
+            pass
+
+    def attack(self, target: Entity):
+        damage = self.power - target.fighter.defense
+
+        if damage < 0:
+            damage = 1
+
+        target.fighter.take_dmg(self, damage)
